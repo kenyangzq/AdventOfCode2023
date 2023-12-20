1ea97f714e11f958bb0af48c3809e25031632dda
Ken Yang <kenyangzq95@outlook.com>
2023-12-19 16:28:40 -0800

    day 17 and day 18 part 1

---
+++ day17/day17/Program.cs
@@ -0,0 +1,102 @@
+﻿
+public class Program {
+
+    public static void Main(string[] args) {
+        var data = ParseInput("test.txt");
+        var solver = new Solver(data);
+        
+        // Part 1
+        var answer = solver.Solve(0, 0, Direction.Right);
+        Console.WriteLine($"Result: {answer}");
+    }
+
+    private static List<List<int>> ParseInput(string fileName) {
+        var lines = File.ReadAllLines(fileName);
+        var data = lines.Select(l => l.Select(c => int.Parse(c.ToString())).ToList()).ToList();
+        foreach (var line in data)
+        {
+            Console.WriteLine(string.Join(", ", line));
+        }
+
+        return data;
+    }
+
+    public enum Direction {
+        Up = 0,
+        Left = 1,
+        Right = 2,
+        Down = 3,
+    }
+
+    private static Dictionary<Direction, List<int>> DirectionMap = new Dictionary<Direction, List<int>> {
+        {Direction.Up, new List<int> {-1, 0}},
+        {Direction.Left, new List<int> {0, -1}},
+        {Direction.Right, new List<int> {0, 1}},
+        {Direction.Down, new List<int> {1, 0}},
+    };
+
+    public class Solver {
+        List<List<int>> matrix;
+        int width;
+        int height;
+        const int StraightCountLimit = 3;
+
+        public Solver(List<List<int>> matrix) {
+            this.matrix = matrix;
+            this.width = matrix[0].Count;
+            this.height = matrix.Count;
+        }
+
+        // Dijkstra's algorithm
+        public int Solve(int row, int col, Direction dir) {
+            // row, col, direction, straightCount, valueCount
+            PriorityQueue<(int, int, Direction, int, int), int> heapq = new PriorityQueue<(int, int, Direction, int, int), int>();
+            HashSet<(int, int, Direction, int)> visited = new();
+
+            heapq.Enqueue((row, col, dir, 1, 0), 0);
+
+            while (heapq.TryDequeue(out var values, out _)) {
+                var (curRow, curCol, curDir, straightCount, valueCount) = values;
+
+                if (curRow == height - 1 && curCol == width - 1) {
+                    return valueCount;
+                }
+
+                visited.Add((curRow, curCol, curDir, straightCount));
+
+                foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+                {
+                    // If straightCount > limit, skip
+                    if (curDir == direction && straightCount >= StraightCountLimit) {
+                        continue;
+                    }
+
+                    // opposite direction not allowed
+                    if ((int)direction + (int)curDir == 3) {
+                        continue;
+                    }
+
+                    var nextRow = curRow + DirectionMap[direction][0];
+                    var nextCol = curCol + DirectionMap[direction][1];
+
+                    // Out of bound
+                    if (nextRow < 0 || nextRow >= height || nextCol < 0 || nextCol >= width) {
+                        continue;
+                    }
+
+                    // next value count
+                    var nextValueCount = valueCount + matrix[nextRow][nextCol];
+
+                    // if has been visited then skip
+                    if (visited.Contains((nextRow, nextCol, direction, straightCount))) {
+                        continue;
+                    }
+                    heapq.Enqueue((nextRow, nextCol, direction, direction == curDir ? straightCount + 1 : 1, nextValueCount), nextValueCount);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
---
+++ day18/day18/Program.cs
@@ -0,0 +1,187 @@
+﻿
+
+public class Program {
+    public static void Main() {
+        var instructions = ParseInput("input.txt");
+
+        var (grid, curx, cury) = MeasureGrid(instructions);
+
+        GridSolver solver = new GridSolver(grid);
+
+        solver.MarkGrid(instructions, curx, cury);
+        
+        // Part 1
+        // Count the boundary-connected .s
+        int count = solver.CountBoundaryConnectedDots();
+        solver.PrintGrid();
+
+        int answer = (grid.Count * grid[0].Count) - count;
+        Console.WriteLine($"Answer: {answer}");
+
+    }
+
+    public enum Direction {
+        R,
+        D,
+        L,
+        U,
+    }
+
+    public static Dictionary<Direction, (int, int)> DirectionMap = new Dictionary<Direction, (int, int)> {
+        {Direction.R, (0, 1)},
+        {Direction.D, (1, 0)},
+        {Direction.L, (0, -1)},
+        {Direction.U, (-1, 0)},
+    };
+
+    public static List<(Direction, int, string)> ParseInput(string fileName) {
+        var input = System.IO.File.ReadAllLines(fileName);
+
+        List<(Direction, int, string)> instructions = new List<(Direction, int, string)>();
+
+        foreach (var line in input) {
+            var values = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Direction d = Enum.Parse<Direction>(values[0]);
+            int value = int.Parse(values[1]);
+            string color = values[2].Replace("(", "").Replace(")", "");
+
+            instructions.Add((d, value, color));
+        }
+
+        return instructions;
+    }
+
+    // Assuming we start from 0, 0,
+    // Return grid and starting point.
+    private static (List<List<string>>, int, int) MeasureGrid(List<(Direction, int, string)> instructions) {
+        int minx = 0;
+        int miny = 0;
+        int maxx = 0;
+        int maxy = 0;
+
+        int curx = 0, cury = 0;
+
+        foreach (var (d, value, color) in instructions) {
+            switch (d) {
+                case Direction.R:
+                    curx += value;
+                    break;
+                case Direction.D:
+                    cury -= value;
+                    break;
+                case Direction.L:
+                    curx -= value;
+                    break;
+                case Direction.U:
+                    cury += value;
+                    break;
+            }
+
+            if (curx < minx) {
+                minx = curx;
+            }
+
+            if (curx > maxx) {
+                maxx = curx;
+            }
+
+            if (cury < miny) {
+                miny = cury;
+            }
+
+            if (cury > maxy) {
+                maxy = cury;
+            }
+        }
+
+        Console.WriteLine($"minx: {minx}, maxx: {maxx}, miny: {miny}, maxy: {maxy}");
+
+        int width = maxx - minx + 1;
+        int height = maxy - miny + 1;
+
+        List<List<string>> grid = new List<List<string>>();
+        for (int i = 0; i < height; i++) {
+            grid.Add(Enumerable.Repeat(".", width).ToList());
+        }
+
+        return (grid, maxy, 0 - minx);
+    }
+
+    private class GridSolver {
+        List<List<string>> grid;
+        int width;
+        int height;
+
+        public GridSolver(List<List<string>> grid) {
+            this.grid = grid;
+            this.width = grid[0].Count;
+            this.height = grid.Count;
+        }
+
+        public void MarkGrid(List<(Direction, int, string)> instructions, int x, int y) {
+            
+            grid[x][y] = "#";
+            foreach (var instruction in instructions) {
+
+                for (int i = 0; i < instruction.Item2; i++) {
+                    x += DirectionMap[instruction.Item1].Item1;
+                    y += DirectionMap[instruction.Item1].Item2;
+                    
+                    if (x < 0 || x >= height || y < 0 || y >= width) {
+                        Console.WriteLine($"Out of bounds: {x}, {y}");
+                        Console.Read();
+                    }
+                    
+                    grid[x][y] = "#";
+                }
+            }
+        }
+
+        public int CountBoundaryConnectedDots() {
+            for (int i = 0; i < height; i++) {
+                MarkBoundaryConnectedDots(0, i);
+                MarkBoundaryConnectedDots(height - 1, i);
+            }
+
+            for (int i = 0; i < width; i++) {
+                MarkBoundaryConnectedDots(i, 0);
+                MarkBoundaryConnectedDots(i, width - 1);
+            }
+
+            int count = 0;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    count += grid[i][j] == "X" ? 1 : 0;
+                }
+            }
+
+            return count;
+        }
+
+        private void MarkBoundaryConnectedDots(int x, int y) {
+            if (x < 0 || x >= height || y < 0 || y >= width) {
+                return;
+            }
+
+            if (grid[x][y] == "#" || grid[x][y] == "X") {
+                return;
+            }
+
+            grid[x][y] = "X";
+
+            MarkBoundaryConnectedDots(x + 1, y);
+            MarkBoundaryConnectedDots(x - 1, y);
+            MarkBoundaryConnectedDots(x, y + 1);
+            MarkBoundaryConnectedDots(x, y - 1);
+        }
+
+        public void PrintGrid() {
+            Console.WriteLine($"width: {width}, height: {height}");
+
+            for (int i = 0; i < height; i++) {
+                Console.WriteLine(string.Join("", grid[i]));
+            }
+        }
+    }
+
+}
